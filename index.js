e881726c94fe616a68e04c48eaec319f6705058d
iammahesh09 <iammahesh.nlr@gmail.com>
2018-06-26 19:51:38 +0530

    Create Basic Basic Callback function

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ var http = require('http');
 
 var server = http.createServer();
 
-server.listen(9000, "Hello NodeJS")
+server.listen(9000, callback)
+
+
+function callback(){
+    console.log("Hello NodeJS")
+}
+
