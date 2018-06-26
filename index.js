6237be8146f48da898258025d414aaf752866010
iammahesh09 <iammahesh.nlr@gmail.com>
2018-06-26 20:16:32 +0530

    Create basic Router

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 var http = require('http');
 
 
-function handleRequest(req, res){
-    console.log(req.url);
-    res.write("Hello NodeJS Web");
-    res.end();
+function handleRequest(req, res) {
+
+    switch (req.url) {
+        case '/':
+            res.write("Hello NodeJS Web");
+            res.end();
+            break;
+
+        case '/users':
+            res.write("Users List");
+            res.end();
+            break;
+
+        case '/products':
+            res.write("Show Products");
+            res.end();
+            break;
+
+        default:
+            res.write("Page Error");
+            res.end();
+            break;
+    }
 }
 
 var server = http.createServer(handleRequest);
- 
-server.listen(9000, callback)
- 
 
-function callback(){
+server.listen(9000, callback)
+
+
+function callback() {
     console.log("Hello NodeJS")
 }
-
