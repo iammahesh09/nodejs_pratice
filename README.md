7ff98677612daf4d2b04c23993a8425d0ab12362
iammahesh09 <iammahesh.nlr@gmail.com>
2018-06-26 19:49:03 +0530

    Create Basic NodeJS Server

---
+++ README.md
@@ -0,0 +1,11 @@
+
+Welcome NodejS
+
+
+Create Nodejs
+
+    var http = require('http');
+
+    var server = http.createServer(handellData);
+
+    server.listen(9000, 'Hello! NodeJs')
--- index.js
+++ index.js
@@ -1,53 +1,5 @@
 var http = require('http');
 
-var server = http.createServer(handellData);
+var server = http.createServer();
 
-function handellData(req, res) {
-
-    switch (req.url) {
-        case '/':
-            res.write('Hello NodeJS');
-            res.end();
-            break;
-
-        case '/user':
-            var usersList = [
-                {
-                    id: 1,
-                    title: "reprehenderit",
-                    body: "est autem sunt rem eveniet architecto"
-                },
-                {
-                    id: 2,
-                    title: "qui est esse",
-                    body: "aperiam non debitis possimus qui neque nisi nulla"
-                },
-                {
-                    id: 3,
-                    title: "ipsa sit aut",
-                    body: "accusantium quis pariatur velit aut"
-                }
-            ];
-            res.write(JSON.stringify(usersList));
-            res.end();
-            break;
-
-        case '/product':
-            res.write('Product List');
-            res.end();
-            break;
-
-        default:
-            res.write('Page Error');
-            res.end();
-            break;
-    }
-
-
-}
-
-server.listen(9000, callback)
-
-function callback() {
-    console.log("Server is running on 9000")
-}
+server.listen(9000, "Hello NodeJS")
