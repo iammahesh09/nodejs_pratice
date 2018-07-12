3309a344789e57a30f4de103bcc924b6a726b314
iammahesh09 <iammahesh.nlr@gmail.com>
2018-07-12 14:59:18 +0530

    Add file in product

--- routers/product.router.js
+++ routers/product.router.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const multer = require('multer')
 
 const routes = express.Router();
 
 var productsCtrl = require('../controllers/products.ctrl');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'FilesDrive')
+    },
+    filename: function (req, file, cb) {
+        let file_name = Date.now() + '-' + file.originalname
+        req.body.model_image = file_name;
+        cb(null, file_name)
+    }
+})
+
+let upload = multer({
+    storage: storage
+})
+
 routes.get('/', productsCtrl.get);
 routes.get('/:id', productsCtrl.getId);
-routes.post('/', productsCtrl.addProduct);
+routes.post('/', upload.single('model_image'), productsCtrl.addProduct);
 routes.delete('/:id', productsCtrl.deleteProduct);
 routes.put('/:id', productsCtrl.updateProduct);
 routes.patch('/:id', productsCtrl.patchData);
