101036a3ab504a1895a6a7f6f58c91401fead091
iammahesh09 <iammahesh.nlr@gmail.com>
2018-07-24 09:01:53 +0530

    config Change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    jwtPassword: 'secret',
-    //connectUrl :'mongodb://admin:products123@ds135061.mlab.com:35061/mobile-products'
-    connectUrl :'mongodb://localhost:27017/products'
+module.exports =  {
+    jwtPassword:'secret', 
+    connectUrl:'mongodb://admin:products123@ds135061.mlab.com:35061/mobile-products'
+    //connectUrl :'mongodb://localhost:27017/products'
 }
