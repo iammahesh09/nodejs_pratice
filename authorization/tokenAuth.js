b181a66948a7b823dd7c1c1b2129281da311d09e
iammahesh09 <iammahesh.nlr@gmail.com>
2018-07-30 19:54:10 +0530

    Headers

--- authorization/tokenAuth.js
+++ authorization/tokenAuth.js
@@ -11,8 +11,10 @@ module.exports = {
                 res.status(400);
                 res.send("Unauthorised")
             } else {
-                //res.header("Access-Control-Allow-Origin", "*");
-                //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+                res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+                res.setHeader('Access-Control-Allow-Credentials', true);
                 next();
             }
         })
